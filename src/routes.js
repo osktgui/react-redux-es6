0e564246a1d6153d5c95a20a036a43470326f139
Luis <luis@xurface.com>
2018-10-30 21:41:28 -0500

    create manage course page

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,14 @@ import App from './components/App';
 import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
 import CoursesPage from './components/course/CoursesPage';
+import ManagerCoursePage from './components/course/ManageCoursePage';
 
 export default (
   <Route path="/" component={ App }>
     <IndexRoute component={ HomePage } />
     <Route path="about" component={ AboutPage } />
     <Route path="courses" component={ CoursesPage } />
+    <Route path="course" component={ ManagerCoursePage } />
+    <Route path="course/:id" component={ ManagerCoursePage } />
   </Route>
 );
