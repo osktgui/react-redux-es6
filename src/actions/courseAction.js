db28b1696316776180a0d3a2135c20315bf18f50
Luis <luis@xurface.com>
2018-11-05 20:58:07 -0500

    Call begin ajax in thunks

--- src/actions/courseAction.js
+++ src/actions/courseAction.js
@@ -1,5 +1,7 @@
 import * as types from './actionTypes';
 import courseApi from '../api/mockCourseApi';
+import { beginAjaxCall } from './ajaxStatusActions';
+
 
 export function loadCoursesSuccess(courses) {
   return { type: types.LOAD_COURSES_SUCCESS, courses };
@@ -15,6 +17,7 @@ export function updateCourseSuccess(course) {
 
 export function loadCourses() {
   return function (dispatch) {
+    dispatch(beginAjaxCall());
     return courseApi.getAllCourses().then(courses => {
       dispatch(loadCoursesSuccess(courses));
     }).catch(error => { throw(error); });
@@ -23,6 +26,7 @@ export function loadCourses() {
 
 export function saveCourse(course) {
   return function (dispatch) {
+    dispatch(beginAjaxCall());
     return courseApi.saveCourse(course).then(savedCourse => {
       course.id ? dispatch(updateCourseSuccess(savedCourse)): dispatch(createCourseSuccess(savedCourse));
     }).catch(error => { throw(error); });
